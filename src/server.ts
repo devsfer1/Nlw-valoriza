f6ba7dc3dc20c92a771a1c57d8ada388892ff65d
devsfer1 <fernando.chaves16@hotmail.com>
2021-06-25 06:12:20 -0300

    configuring server

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/test', (req, res) => {
+  return res.send('Olá Nlw')
+})
+
+app.listen(3000, () => console.log('server is running'))
