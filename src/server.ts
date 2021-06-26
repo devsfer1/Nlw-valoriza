56641d87f94ecc2c0e445a39950e588715df02a6
devsfer1 <fernando.chaves16@hotmail.com>
2021-06-25 23:32:55 -0300

    feat: first post route

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,8 @@ app.get('/test', (req, res) => {
   return res.send('Olá Nlw')
 })
 
+app.post('/test-post', (req, res) => {
+  return res.send('Olá Nlw Post')
+})
+
 app.listen(3000, () => console.log('server is runningg'))
