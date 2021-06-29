3031affe7a104dbfdf693e571b9441900803b48d
devsfer1 <fernando.chaves16@hotmail.com>
2021-06-28 22:16:27 -0300

    feat: handle compliments

---
+++ src/controllers/CreateComplimentController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import { CreateComplimentService } from '../repositories/CreateComplimentService'
+
+
+class CreateComplimentController {
+
+  async handle(req: Request, res: Response) {
+    const { tag_id,
+        user_receiver,
+        user_sender,
+        message } = req.body;
+
+    const createComplimentService = new CreateComplimentService();
+
+    const compliment = await createComplimentService.execute({
+      tag_id,
+      user_receiver,
+      user_sender,
+      message
+    })
+
+    return res.json(compliment);
+  }
+}
+
+export { CreateComplimentController }
